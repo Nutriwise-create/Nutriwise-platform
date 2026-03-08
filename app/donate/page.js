25378a8f3ca05bbeca28c51245b80fb90feb3512
Nutriwise-create <join.nutriwise@outlook.com>
2026-03-08 05:38:50 +0530

    Create page.js

---
+++ app/donate/page.js
@@ -0,0 +1,32 @@
+"use client"
+
+export default function Donate(){
+
+return(
+
+<div style={{textAlign:"center",padding:"80px"}}>
+
+<h1>Support Cancer Nutrition Care</h1>
+
+<p style={{maxWidth:"600px",margin:"20px auto"}}>
+Your support helps NutriWise provide compassionate nutrition
+guidance to cancer patients who may not have access to structured
+nutritional care during treatment and recovery.
+</p>
+
+<button style={{
+background:"#6d4df5",
+color:"white",
+padding:"14px 30px",
+border:"none",
+borderRadius:"25px",
+cursor:"pointer"
+}}>
+Donate Coming Soon
+</button>
+
+</div>
+
+)
+
+}
